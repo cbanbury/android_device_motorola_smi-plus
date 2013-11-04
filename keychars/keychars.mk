25806451945ddc127c013a99463f157b59c03b4a
Patrick Harbers <jgrharbers@gmail.com>
2013-11-04 14:53:09 +0100

    Files already in CM sources

--- keychars/keychars.mk
+++
@@ -1,6 +0,0 @@
-LOCAL_PATH := device/motorola/smi-plus/keychars
-PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/Generic.kcm:system/usr/keychars/Generic.kcm \
-    $(LOCAL_PATH)/qwerty.kcm:system/usr/keychars/qwerty.kcm \
-    $(LOCAL_PATH)/qwerty2.kcm:system/usr/keychars/qwerty2.kcm \
-    $(LOCAL_PATH)/Virtual.kcm:system/usr/keychars/Virtual.kcm \
--- keylayout/keylayout.mk
+++ keylayout/keylayout.mk
@@ -1,11 +1,8 @@
-LOCAL_PATH := device/motorola/smi/keylayout
+LOCAL_PATH := device/motorola/smi-plus/keylayout
 PRODUCT_COPY_FILES += \
-	$(LOCAL_PATH)/AVRCP.kl:system/usr/keylayout/AVRCP.kl \
-	$(LOCAL_PATH)/Generic.kl:system/usr/keylayout/Generic.kl \
 	$(LOCAL_PATH)/intel_short_long_press.kl:system/usr/keylayout/intel_short_long_press.kl \
 	$(LOCAL_PATH)/gpio-keys.kl:system/usr/keylayout/gpio-keys.kl \
 	$(LOCAL_PATH)/KEYPAD.kl:system/usr/keylayout/KEYPAD.kl \
 	$(LOCAL_PATH)/mrst_keypad.kl:system/usr/keylayout/mrst_keypad.kl \
 	$(LOCAL_PATH)/medfield_audio_Intel_R__MID_Audio_Jack.kl:system/usr/keylayout/medfield_audio_Intel_R__MID_Audio_Jack.kl \
-	$(LOCAL_PATH)/mxt224_key_0.kl:system/usr/keylayout/mxt224_key_0.kl \
-	$(LOCAL_PATH)/qwerty.kl:system/usr/keylayout/qwerty.kl
+	$(LOCAL_PATH)/mxt224_key_0.kl:system/usr/keylayout/mxt224_key_0.kl
